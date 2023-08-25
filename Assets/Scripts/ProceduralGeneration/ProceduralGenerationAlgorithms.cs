bfdc3c193e059bd12b41ca9466dfe198afe395c2
brycebohling <96216764+brycebohling@users.noreply.github.com>
2023-08-25 00:40:40 -0700

    commit before wokring on PG

---
+++ Assets/Scripts/ProceduralGeneration/ProceduralGenerationAlgorithms.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGenerationAlgorithms : MonoBehaviour
+{
+    
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
